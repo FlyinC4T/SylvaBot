01b82ae0c4fc7d5b851ce0c0ddf32baf65772924
C4T <a@a>
2025-04-28 04:37:15 +0200

    Add project files.

---
+++ SylvaBot/Methods/Prompt.cs
@@ -0,0 +1,48 @@
+﻿//
+public class Prompt
+{
+    private DiscordSocketClient _client;
+
+    public Prompt(DiscordSocketClient client) => _client = client;
+
+
+    public string UserPrompt(SocketUser promptUser, string prompt)
+    {
+        int promptAttentionLevel = 0;
+        // Declares how the bot will respond to a user prompt.
+        //   0 = No attention
+        //   1 = Low
+        //   2 = Moderate
+        //   3 = High
+
+
+        //int promptAttentionType; //Not used yet
+        // Declares what kind of attention the bot will give to a user prompt.
+        // This will handle if a user is asking for help, or if they are just talking to the bot.
+        // This could also be used to handle if a user is trying to opt-out.
+        //   0 = Normal conversation.
+        //   1 = Asking for help.
+        //   2 = Requesting to Opt-out and delete data.
+        //   3 = First attention - Requesting to Opt-in and start storing data.
+
+        // Test-case for attention level.
+        if (prompt.Contains(_client.CurrentUser.Id.ToString()))
+        {
+            promptAttentionLevel = 3; // High attention
+
+
+            // Test message.
+            return $"Hi <@{promptUser.Id}>!";
+        }
+
+        // No attention
+        else
+            return "";
+
+        // TODO:
+        // AI Logic, etc.
+        // Handling of attention level.
+
+
+    }
+}
---
+++ SylvaBot/main.cs
@@ -0,0 +1,81 @@
+﻿//
+namespace SylvaBot
+{
+    // Main class
+    class Start
+    {
+        private DiscordSocketClient _client;
+
+        public Start() => _client = new DiscordSocketClient();
+
+
+        // Placeholders
+        private string logMessages = "";
+
+        // Main method
+        static async Task Main(string[] args) => await new Start().MainAsync();
+
+        public async Task MainAsync()
+        {
+            if (string.IsNullOrEmpty(Secret.token))
+            {
+                Console.WriteLine("Bot token not found! Make sure DISCORD_BOT_TOKEN is set.");
+                return;
+            }
+
+            _client = new DiscordSocketClient();
+
+            _client.Ready += Ready;
+            _client.Log += LogAsync;
+            _client.MessageReceived += MessageReceivedAsync;
+
+            await _client.LoginAsync(TokenType.Bot, Secret.token);
+            await _client.StartAsync();
+
+
+            // Block the program from exiting
+            await Task.Delay(-1);
+        }
+
+        private Task LogAsync(LogMessage log)
+        {
+            if (log.Severity != LogSeverity.Info)
+                logMessages += $"{log.Severity}  |  {DateTime.Now}  |  {log.Source}\n    {log.Message}\n\n";
+
+            Console.WriteLine(log.ToString());
+            return Task.CompletedTask;
+        }
+
+        private async Task Ready()
+        {
+            //var mainChannel = _client.GetChannel(Variables.MainChannelID) as IMessageChannel;
+            var logChannel = _client.GetChannel(Variables.LogChannelID) as IMessageChannel;
+
+            await logChannel.SendMessageAsync("Sylva Connected.");
+
+            while (true)
+            {
+                await Task.Delay(10000);
+
+                // If we don't have any log messages, skip this iteration.
+                if (logMessages == "") continue;
+
+                await logChannel.SendMessageAsync($"```\n{logMessages}\n```");
+                logMessages = "";
+            }
+        }
+
+        private async Task MessageReceivedAsync(SocketMessage message)
+        {
+            if (message.Author.Id == _client.CurrentUser.Id)
+                return;
+
+            var response = new Prompt(_client).UserPrompt(message.Author, message.Content);
+
+            if (response == "")
+                return;
+
+            await message.Channel.SendMessageAsync(response);
+        }
+    }
+}
